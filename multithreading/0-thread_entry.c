84394621855f7ae6ed4d590b9d5753106a47c690
Lungile6 <l.mabelebel@alustudent.com>
2025-11-09 18:45:07 +0000

    Debug 1 done

--- multithreading/0-thread_entry.c
+++ multithreading/0-thread_entry.c
@@ -1,13 +1,20 @@
+#include <stdio.h>
+#include <pthread.h>
 #include "multithreading.h"
 
 /**
-* thread_entry - Thread entry point
-* @arg: Unused
-*author: Frank Onyema Orji
-* Return: NULL
-*/
+ * thread_entry - Function to be executed by the thread.
+ * @arg: The argument passed to the thread, expected to be a string.
+ *
+ * Return: Always returns NULL.
+ */
 void *thread_entry(void *arg)
 {
-	printf("%s\n", (char *)arg);
-	pthread_exit(NULL);
+    /* Print the string passed as argument */
+    printf("%s\n", (char *)arg);
+    
+    /* End the thread */
+    pthread_exit(NULL);
+
+    return (NULL); /* Return NULL to satisfy the function signature */
 }
